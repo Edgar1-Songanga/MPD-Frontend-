645ef68570b0aee0f354a7330ee06c8faadf57da
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-06-05 16:16:39 +0100

    Add Application type definition in application.types.ts

--- magestade-platform/src/types/application.types.ts
+++ magestade-platform/src/types/application.types.ts
@@ -1,1 +1,12 @@
 // Placeholder for src/types/application.types.ts
+export type Application = {
+  id: string;
+
+  candidateId: string;
+  jobId: string;
+
+  status: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
