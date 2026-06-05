747cdfb43dfcc9825141a07e581ff71eb3f7bf1c
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-06-05 16:04:59 +0100

    Add Job type definition in job.types.ts

--- magestade-platform/src/types/job.types.ts
+++ magestade-platform/src/types/job.types.ts
@@ -1,1 +1,15 @@
 // Placeholder for src/types/job.types.ts
+export type Job = {
+  id: string;
+
+  companyId: string;
+
+  title: string;
+  description: string;
+
+  location?: string;
+  isActive: boolean;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
